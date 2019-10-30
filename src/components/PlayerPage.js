a5a676fff76758421a894ef171a2b3b9a1644c0e
KingZak28 <zakariah28@hotmail.com>
2019-10-29 21:07:15 -0400

    Removing unused modules

--- src/components/PlayerPage.js
+++ src/components/PlayerPage.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import Pusher from "pusher-js";
 
 const NowPlayingBar = styled.div`
   background-color: #505050;
@@ -22,16 +21,6 @@ const ChatContainer = styled.div`
   float: right;
   height: 100%;
 `;
-
-var pusher = new Pusher("5a03379f1046c6a1ea69", {
-  cluster: "us2",
-  forceTLS: true
-});
-
-var channel = pusher.subscribe("my-channel");
-channel.bind("my-event", function(data) {
-  alert(JSON.stringify(data));
-});
 
 class PlayerPage extends React.Component {
   render() {
